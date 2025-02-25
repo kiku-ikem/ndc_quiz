005ee69c77cab830c1326a936934b4af15480128
kiku-ikem <purana.k.m@gmail.com>
2025-02-25 12:29:46 +0900

    fix: environment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  # 新規登録時のみパスワードの存在と8文字以上をチェックする
+  validates :password, presence: true, length: { minimum: 8 }, on: :create
+  
   has_many :favorites, dependent: :destroy
   has_many :favorite_books, through: :favorites, source: :book
   
